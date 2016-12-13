0a78dca5b4215d9ba533fc3c82511bc561960da9
Patrik Ryd <ryd@protonmail.ch>
2016-12-13 14:17:31 +0000

    Patrik: a few changes...

--- patrik/hepp.sh
+++ patrik/hepp.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-pocketsphinx_continuous -hmm /usr/local/share/pocketsphinx/model/en-us/en-us -lm 0625.lm -dict 0625.dic -samprate 16000 -inmic yes -adcdev plughw:1,0 -nfft 2048
+
+pocketsphinx_continuous -hmm /usr/local/share/pocketsphinx/model/en-us/en-us -lm 0625.lm -dict 0625.dic -samprate 16000 -inmic yes -adcdev plughw:1,0 -nfft 2048 -logfn /dev/null
+
+#pocketsphinx_continuous -hmm /usr/local/share/pocketsphinx/model/en-us/en-us -lm 0625.lm -dict 0625.dic -samprate 16000 -inmic yes -adcdev plughw:1,0 -nfft 2048 -keyphrase 'someone'
