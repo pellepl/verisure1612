c0d3706ed0c01a8d747697a32b25d0f41693f079
Peter Andersson <pelleplutt1976@gmail.com>
2016-12-19 22:12:13 +0100

    interpreter working..?

--- verisure_homeass/PyInterpreter.py
+++ verisure_homeass/PyInterpreter.py
@@ -37,119 +37,141 @@ tokens = {
     'off'
     }
 
-sentences = {
+rules = {
     'GET_TEMP':           'is | what | what is | get , (the) , temperature',
-#    'GET_HUMI':           'is | what | what is | get , (the) , humidity',
-##    'IS_DOOR_OPEN':       'is , (the) , door , open',
-#    'IS_DOOR_CLOSED':     'is , (the) , door , closed',
-#    'IS_DOOR_LOCKED':     'is , (the) , door , locked | lock',
-#    'GET_ARM_STATE':      'is | what | what is | get , (the) , system , armed | disarmed',
-#    'IS_DOOR_UNLOCKED':   'is , (the) , door , unlocked | unlock',
-#    'LAMP_ON_HALL':       'turn | switch , on , (the) , hall , lamp',
-#    'LAMP_ON_HALL2':      '(the) , hall , lamp , on',
-#    'LAMP_ON_KITCHEN':    'turn | switch , on , (the) , kitchen , lamp',
-#    'LAMP_ON_KITCHEN2':   '(the) , kitchen , lamp , on',
+    'GET_HUMI':           'is | what | what is | get , (the) , humidity',
+    'IS_DOOR_OPEN':       'is , (the) , door , open',
+    'IS_DOOR_CLOSED':     'is , (the) , door , closed',
+    'IS_DOOR_LOCKED':     'is , (the) , door , locked | lock',
+    'GET_ARM_STATE':      'is | what | what is | get , (the) , system , armed | disarmed',
+    'IS_DOOR_UNLOCKED':   'is , (the) , door , unlocked | unlock',
+    'LAMP_ON_HALL':       'turn | switch , on , (the) , hall , lamp',
+    'LAMP_ON_HALL2':      '(turn) , (switch) , (the) , hall , lamp , on',
+    'LAMP_ON_KITCHEN':    'turn | switch , on , (the) , kitchen , lamp',
+    'LAMP_ON_KITCHEN2':   '(turn) , (switch) , (the) , kitchen , lamp , on',
+    'ARM':                'arm , (the) , system , (please)',
+    'ARM2':               'system , arm',
+    'DISARM':             'disarm , (the) , system',
+    'DISARM2':            'system , disarm',
 }
 
+isCompiled = False
 compiled = {}
 
 def compileGrammar():
-    for cmd in sentences:
-        defs = sentences[cmd].split()
-        csentence = []
-        
+    for rule in rules:
+        defs = rules[rule].split()
+        cgrammar = []
+
         branch = []
         seq = []
-        for t in defs:
-            if t == '|':
+        for token in defs:
+            if token == '|':
                 branch.append(seq)
                 seq = []
                 continue
-            if t == ',':
+            if token == ',':
                 branch.append(seq)
-                csentence.append(branch)
+                cgrammar.append(branch)
                 seq = []
                 branch = []
                 continue
-            ut = strip(t, '()')
-            opt = len(ut) == len(t) - 2
-            
+            ut = strip(token, '()')
+            opt = len(ut) == len(token) - 2
+
             if not ut in tokens:
-                print 'bad token "' + ut + '" in ' + cmd + ' : ' + sentences[cmd]
+                print 'bad token "' + ut + '" in ' + rule + ' : ' + rules[rule]
                 raise Exception('bad token "' + ut + '"')
             word = {'tok': ut, 'opt':opt}
             seq.append(word)
         if len(seq) > 0:
             branch.append(seq)
         if len(branch) > 0:
-            csentence.append(branch)
-        compiled[cmd] = csentence
+            cgrammar.append(branch)
+        compiled[rule] = cgrammar
     pass
 
 
-_dbg = True
-def matchSentence(words, sentence):
-    if _dbg: print "MATCH " + str(words) + " AGAINST " + str(sentence)
-    wordIx = 0
-    for branch in sentence:
-        if _dbg: print '  BRANCH ' + str(branch)
-        curWordIx = wordIx
-        branchMatch = False
-        for sequence in branch:
-            if _dbg: print '    SEQ ' + str(sequence)
-            seqIx = 0
-            while seqIx < len(sequence):
-                gword = sequence[seqIx]
-                if _dbg: print '      ' + gword['tok'] + ' == ' + words[curWordIx] + '   seq:' + str(seqIx) + 'of' + str(len(sequence)) + '  in:' + str(curWordIx) + 'of' + str(len(words))
-                if words[curWordIx] == gword['tok']:
-                    if _dbg: print '    ok'
-                    curWordIx += 1
-                    seqIx += 1
-                elif gword['opt']:
-                    if _dbg: print '    pass'
-                    seqIx += 1
-                else:
-                    if _dbg: print '    fail'
-                    break
-                
-            if seqIx == len(sequence):
-                if _dbg: print '    SEQUENCE MATCH'
-                wordIx = curWordIx
-                branchMatch = True
-                break
-            else:
-                if _dbg: print '    SEQUENCE FAIL'
-        if not branchMatch:
-            return False
-            
-            
-    return True
-                
+_dbg = False #True
+
+def matchSequence(words, wordIx, sequence):
+    if _dbg: print '    SEQ ' + str(sequence)
+    seqIx = 0
+    while seqIx < len(sequence) and wordIx < len(words):
+        gword = sequence[seqIx]
+        if _dbg: print '      ' + words[wordIx] + ' == ' + gword['tok'] + \
+        '   seq:' + str(seqIx) + '/' + str(len(sequence)) + \
+        '   inp:' + str(wordIx) + '/' + str(len(words))
+        if words[wordIx] == gword['tok']:
+            if _dbg: print '    ok'
+            wordIx += 1
+            seqIx += 1
+        elif gword['opt']:
+            if _dbg: print '    pass'
+            seqIx += 1
+        else:
+            if _dbg: print '    fail'
+            break
+    if seqIx == len(sequence) or wordIx == len(words) and sequence[-1]['opt']:
+        if _dbg: print '    SEQUENCE MATCH'
+        return {'match':True, 'wordIx':wordIx}
+    else:
+        if _dbg: print '    SEQUENCE FAIL'
+        return {'match':False, 'wordIx':0}
+    pass
+
+def matchBranch(words, wordIx, branches, branchIx, match):
+    if branchIx >= len(branches) or match['match']: 
+        if _dbg: print '  BRANCH MATCH'
+        match['match'] = True
+        return
+    if _dbg: print '  BRANCH ' + str(branches[branchIx])
+    for sequence in branches[branchIx]:
+        res = matchSequence(words, wordIx, sequence)
+        if res['match'] and not match['match']:
+            matchBranch(words, res['wordIx'], branches, branchIx + 1, match)
+    if _dbg: print '  BRANCH FAIL'
+
+def matchRule(words, wordIx, branches, branchIx):
+    match = {'match':False}
+    matchBranch(words, wordIx, branches, branchIx, match)
+    if match['match']:
+        if _dbg: print 'RULE MATCH'
+        return True
+    else:
+        if _dbg: print 'RULE FAIL'
+        return False
+
 
 def interpret(speech):
+    global isCompiled
+    if not isCompiled:
+        compileGrammar()
+        isCompiled = True
     words = speech.split()
     if not words[0] == 'veryshor':
-        return 
-    for c in compiled:
-        if matchSentence(words[1:], compiled[c]):
-            print c
+        return
+    if _dbg: print "MATCH " + str(words[1:])
+    for rule in compiled:
+        if _dbg: print "RULE " + str(rule)
+        if matchRule(words[1:], 0, compiled[rule], 0):
+            print rule
             return
     print '?'
-    
-    
-# <COMMAND> : 
-# sentence[
+
+# <COMMAND> :
+# rule[
 #   branch[
 #     seq: [ ord{ tok: <TOKEN> opt: True|False } ] /seq
 #   ] /branch
-# ] /sentence
+# ] /rule
 #
 
 def dump():
     for c in compiled:
-        sentence = compiled[c]
+        rule = compiled[c]
         print 'COMMAND: ' + c
-        for branch in sentence:
+        for branch in rule:
             for sequence in branch:
                 for word in sequence:
                     print '      ' + word["tok"]
@@ -158,5 +180,5 @@ def dump():
 
 if __name__ == "__main__":
     compileGrammar()
+    #dump()
     interpret(sys.argv[1])
-    
